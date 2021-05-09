62fec848f0cae5018723dcb9511928ba730cdbe4
hieuit123 <43605814+hieuit123@users.noreply.github.com>
2021-05-10 00:16:16 +0700

    Fix load words data

--- src/components/biz-smart/WordBook-Detail/RightPanel/index.js
+++ src/components/biz-smart/WordBook-Detail/RightPanel/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import axios from 'axios'
 
 import Word from './Word'
-
+import * as actions from './../../../../actions'
 class RightPanel extends Component {
     constructor(props) {
         super(props);
     }
-
+    componentWillUnmount(){
+        this.props.callWordsMange(actions.initWordsDataAction(null))
+    }
     render() {
+        const initWordsData = async ()=>{
+            let result = await axios.get("/word/getallbyidwordbook/" + this.props.id)
+            let finalResult = result.data
+            if (finalResult.status) this.props.callWordsMange(actions.initWordsDataAction(finalResult.data))
+        }
+
         let myListWord
         if (this.props.wordsManage.wordsData) {
             let tmp_wordsData = this.props.wordsManage.wordsData
@@ -16,6 +25,10 @@ class RightPanel extends Component {
                 return <Word key={index} id={word.W_Id} ipa={word.W_ipaWord} title={word.W_originalWord} example={word.W_Phrase} avatar={word.W_Avatar} translation={word.W_translatedWord} idState={word.W_idState} />
             })
         }
+        else{
+            initWordsData()            
+        }
+
         return (
             <div className="right-panel-wb-detail">
                 {myListWord}
@@ -29,5 +42,11 @@ const mapStateToProps = (state) => {
     }
 }
 
-
-export default connect(mapStateToProps,null)(RightPanel)
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        callWordsMange: (action)=>{
+            dispatch(action)
+        }
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(RightPanel)
--- src/components/biz-smart/WordBook-Detail/index.js
+++ src/components/biz-smart/WordBook-Detail/index.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 
 import LeftPanel from './LeftPanel'
 import RightPanel from './RightPanel'
 import ToolBar from './ToolBar'
-import * as actions from './../../../actions'
 
-class WordBookDetail extends Component {
-    componentWillUnmount(){
-        this.props.callWordsMange(actions.initWordsDataAction(null))
-    }
+export default class WordBookDetail extends Component {
     render() {
-        
         return (
             <div>
                 <ToolBar id={this.props.id}/>
@@ -25,12 +19,3 @@ class WordBookDetail extends Component {
         )
     }
 }
-
-const mapDispatchToProps = (dispatch) =>{
-    return {
-        callWordsMange: (action)=>{
-            dispatch(action)
-        }
-    }
-}
-export default connect(null,mapDispatchToProps)(WordBookDetail)
